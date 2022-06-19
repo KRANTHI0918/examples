7b3762434a9168b79da2c7bae54a4725cdc39caa
root <airtel@aveshasystems.com>
2022-06-20 05:10:32 +0530

    kind.sh on macos & calico addition

--- kind/clean.sh
+++ kind/clean.sh
@@ -1,8 +1,10 @@
+cat >myscript.sh <<'EOF'
 #!/usr/bin/env bash
 
 clean() {
     local a=${1//[^[:alnum:]]/}
     echo "${a,,}"
 }
+EOF
 
-clean Kr@nthi@30%&*()
+clean '"kind-"-("controller") & echo "kind-"-(worker-1 worker-2)'
